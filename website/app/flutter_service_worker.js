c58ee0788b91ea267992b48a62785111730bab48
BocamoCM <bocamo@alumnatflorifa.es>
2026-05-11 09:29:01 +0200

    feat(telemetry): UA personalizado + emojis de plataforma en Discord

--- website/app/flutter_service_worker.js
+++ website/app/flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "95ce254a34f82bbd40d33723a6b2c127
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "deb0c2e3688688b3d34bc091b9f19ee4",
+"flutter_bootstrap.js": "3f4608dede35c7e2aa7a3238efc3ad10",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
